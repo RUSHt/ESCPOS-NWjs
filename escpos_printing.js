c0b590e8bb97d87246a6e3993b4f2d5e55040bd4
RUSHt <petera@rush.technology>
2017-09-26 19:50:57 +0100

    added

--- escpos_printing.js
+++ escpos_printing.js
@@ -188,7 +188,7 @@ var foundprinter = false;
 
         ESCPOS_RESULT = "";
         
-        var file = fileSys.readFileSync(filename);
+        
 
         send();
 
@@ -197,6 +197,7 @@ var foundprinter = false;
             if ( !printQ[0] ) { return; }
             if ( printQ[0].sending == true ) { return; }
             printQ[0].sending = true;
+            var file = fileSys.readFileSync(printQ[0].filename);
             serialPorts.Printer.send(file.buffer,(resp) => { log('done '+filename); printQ.splice(0,1); send(); typeof cB == 'function' && cB({ result: resp, ESCPOS_RESULT: ESCPOS_RESULT, file }); });
         }
 
