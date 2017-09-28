2dd6aca6572adc356426e48f3688e7902f8eb799
RUSHt <petera@rush.technology>
2017-09-28 15:56:28 +0100

    added

--- escpos_printing.js
+++ escpos_printing.js
@@ -48,6 +48,7 @@
 // MODULES "GLOBAL" SECTION
 var fileSys = require('fs');
 var operatingSys = require('os');
+var stream = require('stream');
 
 
 var serialPorts;
@@ -824,6 +825,115 @@ exports.STAR_CP ={
         Thai_CC17 : new Buffer('1B1D7465','hex').toString('utf8'),
         Thai_CC18 : new Buffer('1B1D7466','hex').toString('utf8')
 }
+exports.printImageEpson = function(canvas, cB){
+    let png = new PNG({
+      filterType: 4
+    });
+
+    var image = canvas.toDataURL();
+
+    var filename = __dirname+'/print-canvas-'+Date.now()+'.png';
+
+    fileSys.appendFileSync(filename, new Buffer(image.replace(/^data:image\/\w+;base64,/, ""),'base64') ,'binary');
+
+    log('Written to '+filename);
+
+    fileSys.createReadStream(filename)
+      .pipe(png)
+      .on('parsed', function() {
+        exports._printImageBufferEpson(this.width, this.height, this.data, function(buff){
+          //callback(buff); 
+          log('Got Buffer');
+          serialPorts.Printer.send(buff.buffer,(resp) => { log('Done '+filename); typeof cB == 'function' && cB({ result: resp }); });
+        });
+      })
+      .on('error', function(err) {
+        console.error(err);
+      });
+  },
+
+
+exports._printImageBufferEpson: function(width, height, data, callback){
+    // Get pixel rgba in 2D array
+    var pixels = [];
+    for (var i = 0; i < height; i++) {
+      var line = [];
+      for (var j = 0; j < width; j++) {
+        var idx = (width * i + j) << 2;
+        line.push({
+          r: data[idx],
+          g: data[idx + 1],
+          b: data[idx + 2],
+          a: data[idx + 3]
+        });
+      }
+      pixels.push(line);
+    }
+
+
+    var imageBuffer_array=[];
+    for (var i = 0; i < height; i++) {
+      for (var j = 0; j < Math.ceil(width/8); j++) {
+        var byte = 0x0;
+        for (var k = 0; k < 8; k++) {
+          var pixel = pixels[i][j*8 + k];
+
+          // Image overflow
+          if(pixel === undefined){
+            pixel = {
+              a: 0,
+              r: 0,
+              g: 0,
+              b: 0
+            };
+          }
+
+          if(pixel.a > 126){ // checking transparency
+            var grayscale = parseInt(0.2126 * pixel.r + 0.7152 * pixel.g + 0.0722 * pixel.b);
+
+            if(grayscale < 128){ // checking color
+              var mask = 1 << 7-k; // setting bitwise mask
+              byte |= mask; // setting the correct bit to 1
+            }
+          }
+        }
+
+        imageBuffer_array.push(byte);
+        // imageBuffer = Buffer.concat([imageBuffer, new Buffer([byte])]);
+      }
+    }
+
+    let imageBuffer = Buffer.from(imageBuffer_array);
+
+    // Print raster bit image
+    // GS v 0
+    // 1D 76 30 m   xL xH   yL yH d1...dk
+    // xL = (this.width >> 3) & 0xff;
+    // xH = 0x00;
+    // yL = this.height & 0xff;
+    // yH = (this.height >> 8) & 0xff;
+    // https://reference.epson-biz.com/modules/ref_escpos/index.php?content_id=94
+
+    // Check if width/8 is decimal
+    if(width%8 != 0) {
+      width += 8;
+    }
+
+    append(new Buffer ([0x1d, 0x76, 0x30, 48]));
+    append(new Buffer ([(width >> 3) & 0xff]));
+    append(new Buffer ([0x00]));
+    append(new Buffer ([height & 0xff]));
+    append(new Buffer ([(height >> 8) & 0xff]));
+
+    // append data
+    append(imageBuffer);
+
+    // Don't forget to clean the buffer
+    let buff = buffer;
+    buffer = null;
+    callback(buff);
+    return buff;
+  }
 //=====================================================================================================================================
 
 //LICENSE =============================================================================================================================
